e4a937395f311c5bad49edf79de38a32187b9928
alessandragrazielle <61811422+alessandragrazielle@users.noreply.github.com>
2020-08-27 21:01:03 -0300

    Add files via upload

---
+++ Lista de Exercícios 03/F3_Q19_valordeS3.py
@@ -0,0 +1,16 @@
+n = int(input('Valor de N: '))
+contador = 1
+x = n
+s = 0
+
+while contador <= n:
+    if contador % 2 == 0:
+        s = s - (x/contador)
+    else:
+        s = s + (contador/x)
+
+    contador += 1
+    x -= 1
+
+print(f'S = {s}')
+
