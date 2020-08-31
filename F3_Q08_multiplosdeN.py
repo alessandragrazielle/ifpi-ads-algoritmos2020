e956cec7c4365a793784163c4f28adc7db38f59d
alessandragrazielle <61811422+alessandragrazielle@users.noreply.github.com>
2020-08-31 18:42:30 -0300

    Delete F3_Q08_multiplosdeN.py

--- F3_Q08_multiplosdeN.py
+++
@@ -1,9 +0,0 @@
-n = int(input('Número: '))
-li = int(input('Limite Inferior: '))
-ls = int(input('Limite Superior: '))
-
-
-for a in range (li, ls+1):
-    if li%n == 0:
-        print(li, end=' ')
-    li += 1
