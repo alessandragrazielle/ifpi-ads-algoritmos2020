61f0ea7f3e1bf9118c6c5be0afc4e95abd089422
alessandragrazielle <61811422+alessandragrazielle@users.noreply.github.com>
2020-09-17 11:10:37 -0300

    Update criptografia2.py

--- String - exercícios/criptografia2.py
+++ String - exercícios/criptografia2.py
@@ -7,8 +7,8 @@ def main():
 
         # passo 1
         for letra in msg:
-            alfabeto_min = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-            alfabeto_mai = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+            alfabeto_min = 'abcdefghijklmnopqrstuvwxyz'
+            alfabeto_mai = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
             
             if letra in alfabeto_min or letra in alfabeto_mai:
                 posicao = ord(letra)
